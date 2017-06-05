e1aedd388c402ce717aaa73138828444299e37fe
Arnab Borah <Arnab Borah>
2017-06-04 18:56:47 -0700

    code for user ratings

--- psl-demo-updown/psl-demo/rating_measure/userrating.py
+++ psl-demo-updown/psl-demo/rating_measure/userrating.py
@@ -1,4 +1,4 @@
-import csv
+import csv,sys,os
 
 class userrating:
 	def __init__(self):
@@ -53,13 +53,79 @@ class userrating:
 		temp=self.huge[userid]
 		return temp[self.attrs[subredditid]]
 
+	def rating(self,user,subredditid):		#this function will compute the rating between a user and a subreddit
+		possent,negsent=self.lookupsentiment(user,subredditid)
+		possent=float(possent)
+		negsent=float(negsent)
+		updown=int(self.lookupdown(user,subredditid))
+		nocs=int(self.retrieve_noc(user,subredditid))
+		#rating from the comments
+		#print("possent/negsent/updown/no.of.comments",possent,negsent,updown,nocs)
+		if nocs>0:
+			rating_comment=0.5+(nocs)*0.1			#bias of 0.5
+		else:
+			rating_comment=0
+		rating_comment=min(1,rating_comment)
 
+		#rating from the upvotes
+		if updown==0:
+			rating_votes=0
+		else:
+			if updown>0:
+				rating_votes=0.5+updown*0.2			#the values of 20 should be the mean of all values, it will be replaced later
+			else:
+				rating_votes=0.5+updown*(-0.2)
+		rating_votes=min(1,rating_votes)
 
+		#rating from the sentiment
+		if possent>negsent:
+			rating_senti=1
+		else:
+			rating_senti=-1
+
+		avgrating=(rating_comment+rating_votes+rating_senti)/3
+		return avgrating
+		#print("three ratings",rating_senti,rating_votes,rating_comment)
+		#print(user,subredditid,avgrating)
+
+	def drawProgressBar(self,percent, barLen = 50):			#just a progress bar so that you dont lose patience
+		    sys.stdout.write("\r")
+		    progress = ""
+		    for i in range(barLen):
+		        if i<int(barLen * percent):
+		            progress += "="
+		        else:
+		            progress += " "
+		    sys.stdout.write("[ %s ] %.2f%%" % (progress, percent * 100))
+		    sys.stdout.flush()
+
+	def findallratings(self):
+		#outf=open('allratings.txt','w')
+		csvfile1=open('allratings.csv', 'w',newline='')
+		csvfile=csv.writer(csvfile1,delimiter=',')
+		ofilex=open('pairsnotfound.txt','w')
+		infile1=open('../users_obs.txt','r')
+		infile2=open('../subreddits_obs.txt','r')
+		inread1=infile1.read()
+		inread2=infile2.read()
+		allusers=inread1.split()
+		allsubreddits=inread2.split()
+		allpairs=[(a,b) for a in allusers for b in allsubreddits]
+		lenx=len(allpairs)
+		for j in range(lenx):
+			el=allpairs[j]
+			#print(el)
+			try:
+				rating=self.rating(el[0],el[1])
+				temp=[el[0],el[1],rating]
+				csvfile.writerow(temp)
+			except:
+				continue;
+			self.drawProgressBar((j+1)/lenx)
 
 uu=userrating()
 uu.loadnoc()
-print(uu.retrieve_noc('10-Dec','t5_6'))
 uu.loadupdowns()
-print(uu.lookupdown('25462y7asdfh','t5_6'))
 uu.loadsentiments()
-print(uu.lookupsentiment('25462y7asdfh','t5_6'))
+print(uu.rating('911_was_an_insidejob','t5_6'))
+uu.findallratings()
